fe9d088cce003de3f0bec4016b0ad90a55459427
muthuram05 <mr032495@gmail.com>
2025-09-05 00:07:59 +0530

    domain url for production

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,3 +1,3 @@
-export const DOMAIN_URL = "http://127.0.0.1:5001/"
+// export const DOMAIN_URL = "http://127.0.0.1:5001/"
 
-// export const DOMAIN_URL = "https://vishga-backend-ysf0.onrender.com/"
+export const DOMAIN_URL = "https://vishga-backend-ysf0.onrender.com/"
